ac23c82d15378ff7ae724de7b1338dc09904146b
Lugtu, Jihan Tyler <1931638@00-63157.local>
2018-10-15 08:21:07 -0700

    Initial Commit

---
+++ Lab10_ToManyFunctions/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Lab10_ToManyFunctions
+//
+//  Created by Lugtu, Jihan Tyler on 10/15/18.
+//  Copyright © 2018 Lugtu, Jihan Tyler. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
